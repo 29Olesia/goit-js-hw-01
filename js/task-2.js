1035f20c57600257675d1a1868c98268486ceadd
29Olesia <148815850+29Olesia@users.noreply.github.com>
2024-05-25 17:01:27 -0400

    Update task-2.js

--- js/task-2.js
+++ js/task-2.js
@@ -1,9 +1,14 @@
-function getShippingMessage(country, price, deliveryFee) {
-    const totalPrice = price + deliveryFee;
+function getElementWidth(content, padding, border) {
+    const contentWidth = parseFloat(content);
+    const paddingWidth = parseFloat(padding);
+    const borderWidth = parseFloat(border);
 
-    return `Shipping to ${country} will cost ${totalPrice} credits`;
+    const totalWidth = contentWidth + 2 * paddingWidth + 2 * borderWidth;
+
+    return totalWidth;
   }
 
-  console.log(getShippingMessage("Australia", 120, 50)); 
-  console.log(getShippingMessage("Germany", 80, 20)); 
-  console.log(getShippingMessage("Sweden", 100, 20)); 
+  console.log(getElementWidth("50px", "8px", "4px")); 
+  console.log(getElementWidth("60px", "12px", "8.5px")); 
+  console.log(getElementWidth("200px", "0px", "0px")); 
+
