98e60b290cb9f91672762a84428db17d5314f69a
29Olesia <148815850+29Olesia@users.noreply.github.com>
2024-05-25 17:00:52 -0400

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -1,20 +1,9 @@
-document.addEventListener('DOMContentLoaded', () => {
-    function makeTransaction(quantity, pricePerDroid) {
-        const totalPrice = quantity * pricePerDroid;
-        return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
-    }
+function makeTransaction(quantity, pricePerDroid) {
+    const totalPrice = quantity * pricePerDroid;
+    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+}
 
-    const task1Results = document.getElementById('task-1-results');
 
-    const results1 = [
-        makeTransaction(5, 3000),
-        makeTransaction(3, 1000),
-        makeTransaction(10, 500)
-    ];
-
-    results1.forEach(result => {
-        const p = document.createElement('p');
-        p.textContent = result;
-        task1Results.appendChild(p);
-    });
-});
+console.log(makeTransaction(5, 3000)); 
+console.log(makeTransaction(3, 1000)); 
+console.log(makeTransaction(10, 500)); 
