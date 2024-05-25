0544abc5630fbab3124ea081912f56eccd1d176a
29Olesia <148815850+29Olesia@users.noreply.github.com>
2024-05-25 17:01:09 -0400

    Update task-2.js

--- js/task-2.js
+++ js/task-2.js
@@ -1,20 +1,9 @@
-document.addEventListener('DOMContentLoaded', () => {
-  function getShippingMessage(country, price, deliveryFee) {
-      const totalPrice = price + deliveryFee;
-      return `Shipping to ${country} will cost ${totalPrice} credits`;
+function getShippingMessage(country, price, deliveryFee) {
+    const totalPrice = price + deliveryFee;
+
+    return `Shipping to ${country} will cost ${totalPrice} credits`;
   }
 
-  const shippingResults = document.getElementById('shipping-results');
-
-  const messages = [
-      getShippingMessage("Australia", 120, 50),
-      getShippingMessage("Germany", 80, 20),
-      getShippingMessage("Sweden", 100, 20)
-  ];
-
-  messages.forEach(message => {
-      const p = document.createElement('p');
-      p.textContent = message;
-      shippingResults.appendChild(p);
-  });
-});
+  console.log(getShippingMessage("Australia", 120, 50)); 
+  console.log(getShippingMessage("Germany", 80, 20)); 
+  console.log(getShippingMessage("Sweden", 100, 20)); 
