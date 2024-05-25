c66a80e0426f5a55faf15f16310e78fe52e231d5
29Olesia <148815850+29Olesia@users.noreply.github.com>
2024-05-25 17:01:56 -0400

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,25 +1,14 @@
-document.addEventListener('DOMContentLoaded', () => {
-  function getElementWidth(content, padding, border) {
-      const contentWidth = parseFloat(content);
-      const paddingWidth = parseFloat(padding);
-      const borderWidth = parseFloat(border);
+function getElementWidth(content, padding, border) {
+    const contentWidth = parseFloat(content);
+    const paddingWidth = parseFloat(padding);
+    const borderWidth = parseFloat(border);
 
-      const totalWidth = contentWidth + 2 * paddingWidth + 2 * borderWidth;
+    const totalWidth = contentWidth + 2 * paddingWidth + 2 * borderWidth;
 
-      return totalWidth;
+    return totalWidth;
   }
 
-  const elementWidthResults = document.getElementById('element-width-results');
+  console.log(getElementWidth("50px", "8px", "4px")); 
+  console.log(getElementWidth("60px", "12px", "8.5px")); 
+  console.log(getElementWidth("200px", "0px", "0px")); 
 
-  const widths = [
-      getElementWidth("50px", "8px", "4px"),
-      getElementWidth("60px", "12px", "8.5px"),
-      getElementWidth("200px", "0px", "0px")
-  ];
-
-  widths.forEach(width => {
-      const p = document.createElement('p');
-      p.textContent = `Element width: ${width}px`;
-      elementWidthResults.appendChild(p);
-  });
-});
